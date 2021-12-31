54b2a03def2cd3bd45a454594b19b63a44ff34e0
hafizulf <hafizulf68@gmail.com>
2021-12-31 08:59:32 +0700

    add kriteria migration

---
+++ app/Database/Migrations/2021-12-31-015400_Kriteria.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Kriteria extends Migration
+{
+  public function up()
+  {
+    $this->forge->addField([
+      'id INT UNSIGNED AUTO_INCREMENT',
+      'nama VARCHAR(64) NOT NULL',
+      'jenis VARCHAR(64) NOT NULL',
+    ]);
+    $this->forge->addKey('id', true);
+    $this->forge->createTable('kriteria');
+  }
+
+  public function down()
+  {
+    $this->forge->dropTable('kriteria');
+  }
+}
