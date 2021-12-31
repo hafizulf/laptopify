f18ce5bbc77d219ebb419d877ed8f6b5d0c64975
hafizulf <hafizulf68@gmail.com>
2021-12-31 10:05:45 +0700

    add nilai_utility migration

---
+++ app/Database/Migrations/2021-12-31-030021_NilaiUtility.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class NilaiUtility extends Migration
+{
+  public function up()
+  {
+    $this->forge->addField([
+      'id INT UNSIGNED AUTO_INCREMENT',
+      'nilai_kriteria_id INT UNSIGNED NOT NULL',
+      'nilai_utility FLOAT NOT NULL',
+    ]);
+    $this->forge->addKey('id', true);
+    $this->forge->addForeignKey('nilai_kriteria_id', 'nilai_kriteria', 'id', 'CASCADE', 'CASCADE');
+    $this->forge->createTable('nilai_utility');
+  }
+
+  public function down()
+  {
+    $this->forge->dropTable('nilai_utility');
+  }
+}
