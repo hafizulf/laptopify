21f934f9f7c55ef306870ed4cce7a361e4a8b03f
hafizulf <hafizulf68@gmail.com>
2021-12-31 09:28:38 +0700

    add normalisasi_bobot migration

---
+++ app/Database/Migrations/2021-12-31-022524_NormalisasiBobot.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class NormalisasiBobot extends Migration
+{
+  public function up()
+  {
+    $this->forge->addField([
+      'id INT UNSIGNED AUTO_INCREMENT',
+      'pembobotan_id INT UNSIGNED NOT NULL',
+      'nilai_normalisasi_bobot INT UNSIGNED NOT NULL DEFAULT "0"',
+    ]);
+    $this->forge->addKey('id', true);
+    $this->forge->addForeignKey('pembobotan_id', 'pembobotan', 'id');
+    $this->forge->createTable('normalisasi_bobot');
+  }
+
+  public function down()
+  {
+    $this->forge->dropTable('normalisasi_bobot');
+  }
+}
