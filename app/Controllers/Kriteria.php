f0145db736744863bc89e8b11580a0c259f2bcc6
hafizulf <hafizulf68@gmail.com>
2022-01-01 13:17:36 +0700

    validate fields

--- app/Controllers/Kriteria.php
+++ app/Controllers/Kriteria.php
@@ -11,4 +11,25 @@ class Kriteria extends BaseController
     $data['judul'] = 'Data Kriteria';
     return view('kriteria/index', $data);
   }
+
+  public function create()
+  {
+    if(!$this->request->isAJAX()) {
+      throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+    }
+
+    $rules = [
+      'nama' => 'required',
+      'jenis' => 'required',
+    ];
+
+    if (!$this->validate($rules)) {
+      $errors  = [
+        'nama' => $this->validation->getError('nama'),
+        'jenis' => $this->validation->getError('jenis'),
+      ];
+
+      echo json_encode(['status' => FALSE, 'errors' => $errors]);
+    }
+  }
 }
