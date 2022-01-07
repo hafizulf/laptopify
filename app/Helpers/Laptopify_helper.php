771b007830853ca119e6df0bb6d9d0e10881b9cd
hafizulf <38736580+hafizulf@users.noreply.github.com>
2022-01-08 00:30:06 +0700

    refCode: validate data in model (#5)

--- app/Helpers/Laptopify_helper.php
+++ app/Helpers/Laptopify_helper.php
@@ -10,24 +10,4 @@ class Laptopify extends BaseController
       throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
     }
   }
-
-  function fieldValidation($rules, $func)
-  {
-    $validation = \Config\Services::validation();
-
-    $rulesSize = count($rules);
-    $rulesConverted = array_keys($rules);
-
-    if (!$func->validate($rules, [])) {
-      $errors = [];
-
-      for ($i = 0; $i < $rulesSize; $i++) {
-        $errors[$rulesConverted[$i]] = $validation->getError($rulesConverted[$i]);
-      }
-
-      return $errors;
-    } else {
-      return TRUE;
-    }
-  }
 }
