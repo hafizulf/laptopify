bb51ebbebe4cf27a9bba8407839ea4f978db40d3
hafizulf <hafizulf68@gmail.com>
2022-01-08 05:19:49 +0700

    modify(footer): to the year the web was developed

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>Copyright &copy; Skuy Ngoding <?= date('Y'); ?></span>
+      <span>Copyright &copy; Skuy Ngoding 2022</span>
     </div>
   </div>
 </footer>
