1590000137fd0c1503d269ae72e2071b55cb3cff
hafizulf <hafizulf68@gmail.com>
2022-01-09 22:05:50 +0700

    update table kriteria

--- app/Database/Migrations/2021-12-31-015400_Kriteria.php
+++ app/Database/Migrations/2021-12-31-015400_Kriteria.php
@@ -12,6 +12,7 @@ class Kriteria extends Migration
       'id INT UNSIGNED AUTO_INCREMENT',
       'nama VARCHAR(64) NOT NULL',
       'jenis VARCHAR(64) NOT NULL',
+      'data_kuantitatif INT NOT NULL',
     ]);
     $this->forge->addKey('id', true);
     $this->forge->createTable('kriteria');
