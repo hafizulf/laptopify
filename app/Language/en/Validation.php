8a27728de6703705e60815687de619d13859bec9
hafizulf <hafizulf68@gmail.com>
2022-01-17 15:24:11 +0700

    modify validation rules message

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -2,7 +2,7 @@
 
 // override core en language system validation or define your own en language validation message
 return [
-  'required' => 'wajib diisi.',
-  'is_unique' => '{field} sudah ada, coba lain.',
-  'is_numeric' => 'harus angka'
+  'required' => 'wajib diisi',
+  'is_unique' => '{field} sudah ada, coba lain',
+  'is_numeric' => 'harus angka',
 ];
