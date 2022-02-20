e1896ce21f54f443b7c16b227372bb9371f6dd5f
hafizulf <hafizulf68@gmail.com>
2022-02-20 16:11:47 +0700

    update seeder

--- app/Database/Seeds/Pembobotan.php
+++ app/Database/Seeds/Pembobotan.php
@@ -9,18 +9,18 @@ class Pembobotan extends Seeder
   public function run()
   {
     $data = [
-      ['kriteria_id' => 1, 'nilai_bobot' => 100],
-      ['kriteria_id' => 2, 'nilai_bobot' => 85],
-      ['kriteria_id' => 3, 'nilai_bobot' => 80],
-      ['kriteria_id' => 4, 'nilai_bobot' => 95],
-      ['kriteria_id' => 5, 'nilai_bobot' => 90],
-      ['kriteria_id' => 6, 'nilai_bobot' => 90],
-      ['kriteria_id' => 7, 'nilai_bobot' => 85],
-      ['kriteria_id' => 8, 'nilai_bobot' => 90],
-      ['kriteria_id' => 9, 'nilai_bobot' => 80],
-      ['kriteria_id' => 10, 'nilai_bobot' => 100],
-      ['kriteria_id' => 11, 'nilai_bobot' => 70],
-      ['kriteria_id' => 12, 'nilai_bobot' => 100],
+      ['id_kriteria' => 1, 'nilai_bobot' => 100],
+      ['id_kriteria' => 2, 'nilai_bobot' => 85],
+      ['id_kriteria' => 3, 'nilai_bobot' => 80],
+      ['id_kriteria' => 4, 'nilai_bobot' => 95],
+      ['id_kriteria' => 5, 'nilai_bobot' => 90],
+      ['id_kriteria' => 6, 'nilai_bobot' => 90],
+      ['id_kriteria' => 7, 'nilai_bobot' => 85],
+      ['id_kriteria' => 8, 'nilai_bobot' => 90],
+      ['id_kriteria' => 9, 'nilai_bobot' => 80],
+      ['id_kriteria' => 10, 'nilai_bobot' => 100],
+      ['id_kriteria' => 11, 'nilai_bobot' => 70],
+      ['id_kriteria' => 12, 'nilai_bobot' => 100],
     ];
 
     $this->db->table('pembobotan')->insertBatch($data);
