cdf950c3b16d8046274ef9536d288eb319e38a02
hafizulf <hafizulf68@gmail.com>
2022-03-12 23:20:33 +0700

    feat(my-profile): update profile

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -7,12 +7,15 @@ use App\Models\Admin\User as UserModel;
 
 class User extends BaseController
 {
+  public function __construct()
+  {
+    $this->model = new UserModel();
+  }
+
   public function index()
   {
-    $userModel = new UserModel();
-
     $username = session('username');
-    $user = $userModel
+    $user = $this->model
       ->join('user_roles', 'users.id_user_role = user_roles.id_user_role')
       ->getWhere(['username' => $username])
       ->getRowArray();
@@ -23,5 +26,32 @@ class User extends BaseController
     ];
 
     return view('user/index', $data);
+  }
+
+  public function getDataById()
+  {
+    if (!$this->request->isAJAX()) {
+      throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+    }
+
+    $id = $this->request->getPost('id');
+    $user = $this->model->getWhere(['id_user' => $id])->getRowArray();
+
+    return $this->response->setJSON($user);
+  }
+
+  public function update()
+  {
+    if (!$this->request->isAJAX()) {
+      throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+    }
+
+    $data = $this->request->getPost();
+
+    if ($this->model->save($data) === FALSE) {
+      return $this->response->setJSON(['status' => FALSE, 'errors' => $this->model->errors()]);
+    } else {
+      return $this->response->setJSON(['status' => TRUE, 'message' => 'Data berhasil diperbarui']);
+    }
   }
 }
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -22,12 +22,75 @@
           </ul>
           <hr class="mb-2 mt-0">
           <div class="text-center">
-            <a href="#" class="card-link">Ubah Profil</a>
+            <a href="#" data-toggle="modal" data-target="#modalBoxUbah" data-id="<?= $data['id_user']; ?>" class="card-link update-profile">Ubah Profil</a>
             <a href="#" class="card-link">Ubah Password</a>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <div class="modal fade" id="modalBoxUbah" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header badge-primary">
+          <h5 class="modal-title text-white"><i class="fa fa-edit"></i> Update Profile</h5>
+        </div>
+
+        <form action="/user/update" method="POST" id="formUbah">
+          <div class="modal-body">
+            <?= csrf_field(); ?>
+            <input type="hidden" name="id_user" id="id_user">
+            <div class="form-group">
+              <label for="nama">Nama</label>
+              <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama">
+              <div class="invalid-feedback"></div>
+            </div>
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+              <div class="invalid-feedback"></div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary btn-simpan">Simpan</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+
 </div>
+<?= $this->endSection(); ?>
+
+<?= $this->section('custom-js'); ?>
+<script>
+  $(document).ready(function() {
+    $('.update-profile').on('click', function(e) {
+
+      let id = $(this).data('id');
+      $.ajax({
+        url: '/user/getDataById',
+        type: 'POST',
+        data: {
+          id: id
+        },
+        success: function(response) {
+          $.each(response, function(key, val) {
+            $('[name="' + key + '"]').val(val)
+          })
+        }
+      });
+    })
+
+    const formUbah = $('#formUbah')
+    formUbah.submit(function(e) {
+      e.preventDefault()
+      requestSaveData(formUbah, '#modalBoxUbah')
+      removeClasses('#formUbah')
+    })
+  })
+</script>
 <?= $this->endSection(); ?>
