c8225d16d04dd81da35f8cb9ebb9ee26ef075c28
hafizulf <hafizulf68@gmail.com>
2022-08-09 18:39:04 +0700

    request products by api

---
+++ app/Controllers/Automatic.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Automatic extends BaseController
+{
+  public function index()
+  {
+    $data = [
+      'judul' => 'Pencarian Otomatis'
+    ];
+
+    return view('otomatis/index', $data);
+  }
+
+  public function get_data()
+  {
+    $params = $this->request->getGet();
+
+    $maxPrice = isset($params['max_price']) ? $params['max_price'] : 0;
+    $minPrice = isset($params['min_price']) ? $params['min_price'] : 0;
+    $limit = isset($params['limit']) ? $params['limit'] : 20;
+
+    ini_set('max_execution_time', '1200');
+
+    $ch = curl_init("http://127.0.0.1:8000/products?max={$maxPrice}&min={$minPrice}&limit={$limit}");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    sleep(1);
+
+    // Save products to a json file
+    $products = json_encode(json_decode($response), JSON_PRETTY_PRINT);
+    $file_path = 'data/products.json';
+    file_put_contents($file_path, $products);
+  }
+}
---
+++ app/Views/otomatis/index.php
@@ -0,0 +1,42 @@
+<?= $this->extend('template/content'); ?>
+
+<?= $this->section('content'); ?>
+
+
+<div class="section">
+  <div class="row">
+    <div class="col-md-12">
+
+      <h2 class="text-gray-900"><?= $judul; ?></h2>
+
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <form action="/otomatis/get-data" method="GET">
+            <small class="text-sm text-info">Default tidak ada harga minimum dan maksimum</small>
+            <div class="form-group">
+              <label for="min_price">Harga Minimum</label>
+              <input type="number" class="form-control" name="min_price" id="min_price" placeholder="Harga minimum...">
+            </div>
+            <div class="form-group">
+              <label for="max_price">Harga Maksimum</label>
+              <input type="number" class="form-control" name="max_price" id="max_price" placeholder="Harga maksimum...">
+            </div>
+            <div class="form-group">
+              <label for="limit">Limit <small class="text-sm badge badge-primary">default 20</small></label>
+              <input type="number" class="form-control" name="limit" id="limit" placeholder="Limit Data" min="1" max="100">
+            </div>
+            <button type="submit" class="btn btn-primary btn-simpan"><i class="fas fa fa-spinner"></i> Proses</button>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<?= $this->endSection(); ?>
