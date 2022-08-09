ff4a601fdae1896556397b241be45c42a613e204
hafizulf <hafizulf68@gmail.com>
2022-08-09 22:32:34 +0700

    add subkriteria & UI waiting screen

--- app/Controllers/Automatic.php
+++ app/Controllers/Automatic.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use Exception;
 
 class Automatic extends BaseController
 {
@@ -17,36 +18,46 @@ class Automatic extends BaseController
 
   public function get_data()
   {
-    $maxPrice = $this->request->getGet('max_price');
-    $minPrice = $this->request->getGet('min_price');
-    $limit = $this->request->getGet('limit');
 
-    // Request data
-    ini_set('max_execution_time', '1200');
+    try {
+      $maxPrice = $this->request->getGet('max_price');
+      $minPrice = $this->request->getGet('min_price');
+      $limit = $this->request->getGet('limit');
 
-    $ch = curl_init("http://127.0.0.1:8000/products?max={$maxPrice}&min={$minPrice}&limit={$limit}");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      // Request data
+      ini_set('max_execution_time', '1200');
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+      $ch = curl_init("http://127.0.0.1:8000/products?max={$maxPrice}&min={$minPrice}&limit={$limit}");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    sleep(1);
+      $response = curl_exec($ch);
+      curl_close($ch);
 
-    // Save products to a json file
-    $products = json_encode(json_decode($response), JSON_PRETTY_PRINT);
-    $file_path = 'data/products.json';
-    file_put_contents($file_path, $products);
+      sleep(1);
 
-    sleep(1);
+      // Save products to a json file
+      $products = json_encode(json_decode($response), JSON_PRETTY_PRINT);
+      $file_path = 'data/products.json';
+      file_put_contents($file_path, $products);
 
-    // re-migrate table and seed alternatif data
-    exec('php C:\xampp\htdocs\laptopify\spark migrate:refresh');
+      sleep(1);
 
-    sleep(1);
+      // re-migrate table and seed alternatif data
+      exec('php C:\xampp\htdocs\laptopify\spark migrate:refresh');
 
-    exec('php C:\xampp\htdocs\laptopify\spark db:seed kriteria');
-    exec('php C:\xampp\htdocs\laptopify\spark db:seed subkriteria');
-    exec('php C:\xampp\htdocs\laptopify\spark db:seed pembobotan');
-    exec('php C:\xampp\htdocs\laptopify\spark db:seed alternatif');
+      sleep(1);
+
+      exec('php C:\xampp\htdocs\laptopify\spark db:seed kriteria');
+      exec('php C:\xampp\htdocs\laptopify\spark db:seed subkriteria');
+      exec('php C:\xampp\htdocs\laptopify\spark db:seed pembobotan');
+      exec('php C:\xampp\htdocs\laptopify\spark db:seed alternatif');
+
+      sleep(1);
+
+      // return if successfully
+      echo json_encode(["status" => TRUE]);
+    } catch (Exception $e) {
+      echo json_encode(["status" => FALSE]);
+    }
   }
 }
--- app/Views/otomatis/index.php
+++ app/Views/otomatis/index.php
@@ -16,7 +16,7 @@
     <div class="col-md-6">
       <div class="card">
         <div class="card-body">
-          <form action="/otomatis/get-data" method="GET">
+          <form id="form-proses" action="/otomatis/get-data" method="GET">
             <small class="text-sm text-info">Default tidak ada harga minimum dan maksimum</small>
             <div class="form-group">
               <label for="min_price">Harga Minimum</label>
@@ -30,13 +30,75 @@
               <label for="limit">Limit <small class="text-sm badge badge-primary">default 20</small></label>
               <input type="number" class="form-control" name="limit" id="limit" placeholder="Limit Data" min="1" max="100" value="20">
             </div>
-            <button type="submit" class="btn btn-primary btn-simpan"><i class="fas fa fa-spinner"></i> Proses</button>
+            <p class="text-success text-waiting">
+              Proses pengambilan data secara otomatis membutuhkan waktu, silahkan menunggu beberapa saat...
+            </p>
+            <button type="submit" class="btn btn-primary btn-proses"><i class="fas fa fa-spinner"></i> Proses</button>
           </form>
         </div>
       </div>
 
     </div>
   </div>
+
 </div>
 
 <?= $this->endSection(); ?>
+
+<?php $this->section('custom-js'); ?>
+<script>
+  $(document).ready(function() {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true
+    })
+
+    $('.text-waiting').hide()
+
+    $('.btn-proses').on('click', function(e) {
+      e.preventDefault()
+      $('.text-waiting').show()
+
+      const form = $('#form-proses')
+
+      $.ajax({
+        url: form.attr('action'),
+        type: form.attr('method'),
+        dataType: 'JSON',
+        data: form.serialize(),
+        beforeSend: function() {
+          $('.btn-proses').html('loading.. <span class="spinner-border spinner-border-sm"></span>')
+        },
+        complete: function() {
+          $('.btn-proses').html('<i class="fas fa fa-spinner"></i> Proses')
+        },
+        success: function(response) {
+          if (response.status) {
+            Toast.fire({
+              icon: 'success',
+              title: 'Data berhasil didapatkan!'
+            })
+
+            $('.text-waiting').html('Silahkan lanjut ke proses perhitungan bobot dan nilai')
+          } else {
+            Toast.fire({
+              icon: 'error',
+              title: 'Data gagal didapatkan!'
+            })
+
+            $('.text-waiting').html('Periksa koneksi internet, dan coba beberapa saat lagi').css('color', 'blue')
+          }
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+          alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError)
+        }
+      })
+
+
+    })
+  })
+</script>
+<?php $this->endSection(); ?>
